4ba77a4758ffb2bbcadbb3e5134f14593d6d0ecb
grantmw <grantmwong@gmail.com>
2015-12-18 16:06:54 -0800

    basic show and hide, no animations

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,16 @@
   $(document).ready(function(){
 
-    
+
+    $('.dropdown').on('click','a', function(event){
+      event.preventDefault();
+      console.log(this);
+      $(this).closest('.dd-content').css('display','none');
+    });
+    $('.dropdown').hover(
+      function(){
+        $(this).find('.dd-content').css('display','block');
+      },
+      function(){
+        $(this).find('.dd-content').css('display','none');
+    });
   });
